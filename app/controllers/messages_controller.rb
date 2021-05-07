5022e53ad2d5e44c61c7427d953d4b0392e7da19
MikaArakawa <mika.arakawa@di-v.co.jp>
2021-05-07 11:20:36 +0900

    Active Storageの導入

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,11 @@
 class MessagesController < ApplicationController
   def index
   end
+
+private
+
+def message-params
+  params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+end
+  
 end
